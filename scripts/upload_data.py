780f25c16550a4e8944c95625528dbd218efedfe
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2023-11-29 11:38:16 +0100

    debug for data upload

--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -1,11 +1,16 @@
+import logging
+import sys
 from pathlib import Path
 from typing import Dict, List
 
 from pybis import Openbis
-
-from scripts.utils import get_config
+from utils import get_config, timeit
 
 CONFIG = get_config()
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+LOGGER = logging.getLogger(
+    __name__,
+)
 
 # "/Volumes/compact-20260/*/pdf/Bode - *.pdf"
 # "/Volumes/compact-20260/*/pdf/Bode - AEM-05-093-A-depro-16-3_P1-F-2_1_3336_OA-LCMS-Report.pdf"
@@ -36,50 +41,64 @@ def get_experiments(openbis: Openbis, project: str) -> List[str]:
 
 def get_datasets(openbis: Openbis, experiment: str, dataset_type: str) -> List[str]:
     datasets = openbis.get_datasets(
-        experiment=experiment, type=dataset_type, attrs=["name"]
+        experiment=experiment, type=dataset_type, props=["$NAME"]
     )
-    return datasets.df["name"].tolist()
+    return datasets.df["$NAME"].tolist()
 
 
+@timeit
 def get_all_pdf_files(instrument_dir: Path) -> List[Path]:
     return list(instrument_dir.glob("**/pdf/*.pdf"))
 
 
 if __name__ == "__main__":
     dataset_type = "COMPACT"
-    dataset_ab_dir = Path("/Volumes/compact-20260/")
+    dataset_ab_dir = Path("/Volumes/Bruker-Compact-1/")
 
     openbis = get_openbis(CONFIG)
 
     users = get_all_users(openbis)
+    LOGGER.info(f"There are {len(users)} registered users in openBIS: {users}")
     all_dataset = get_all_pdf_files(dataset_ab_dir)
+    LOGGER.info(f"Found {len(all_dataset)} pdf files in {dataset_ab_dir}")
 
     # space for all users
     # per user project/ experiment
     # per experiment upload dataset, but check if that dataset is already uploaded
     for user in users:
-        user_files = [fn for fn in all_dataset if f"Bode - {user}-" in fn.name]
-        for proj in get_projects(openbis=openbis, user=user):
-            for exp in get_experiments(openbis=openbis, user=user, project=proj):
+        user_files = [
+            fn for fn in all_dataset if f"{user.lower()}" in str(fn.name).lower()
+        ]
+        LOGGER.info(f"Processing user: {user}, has {len(user_files)} pdf files")
+        for proj in get_projects(
+            openbis=openbis, user=user
+        ):  # proj = "/{usr}/projectname/"
+            for exp in get_experiments(
+                openbis=openbis, project=proj
+            ):  # exp = "/{usr}/projectname/experimentname/"
+                data_prefix = "-".join(exp.lower().split("/")[1:])
                 data_names = [
-                    str(fn)
-                    for fn in user_files
-                    if f"Bode - {user}-{proj}-{exp}" in fn.name
+                    str(fn) for fn in user_files if f"Bode - {data_prefix}" in fn.name
                 ]
+                # check if dataset already exists
                 for existing_fn in get_datasets(
                     openbis=openbis, experiment=exp, dataset_type=dataset_type
                 ):
-                    if existing_fn in data_names:
-                        data_names.remove(existing_fn)
-                ds_new = openbis.new_dataset(
-                    type=dataset_type,
-                    experiment=openbis.get_experiment(f"/{user}/{proj}/{exp}"),
-                    files=data_names,
-                    props=[
-                        {
-                            "$name": fn.split("/")[-1].split(".")[0],
-                        }
-                        for fn in data_names
-                    ],
-                )
-                ds_new.save()
+                    for dn in data_names:
+                        if existing_fn in dn:
+                            # if existing_fn in dn, then remove it from data_names
+                            data_names.remove(dn)
+                # upload new datasets
+                for data_name in data_names:
+                    ds_new = openbis.new_dataset(
+                        type=dataset_type,
+                        experiment=openbis.get_experiment(exp),
+                        files=data_name,
+                        props={
+                            "$name": data_name.split("/")[-1].split(".")[0],
+                            "notes": data_name.split("/")[-1]
+                            .split(".")[0]
+                            .split("-")[4],
+                        },
+                    )
+                    ds_new.save()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,17 @@
+import logging
+import sys
+import time
+from functools import wraps
 from pathlib import Path
 
 import yaml  # type: ignore
+from path import CONFIG_PATH
 from pdf2image import convert_from_path
 
-from .path import CONFIG_PATH
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+LOGGER = logging.getLogger(
+    __name__,
+)
 
 
 def pdf2img(pdf_path: Path, img_dir: Path):
@@ -30,3 +38,18 @@ def get_config(config_path: Path = CONFIG_PATH / "config.yaml"):
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
     return config
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        LOGGER.debug(
+            f"Function {func.__name__} {kwargs.keys()} Took {total_time:.4f} seconds = {total_time/60:.1f} mins"
+        )
+        return result
+
+    return timeit_wrapper
